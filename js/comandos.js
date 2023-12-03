a1678adec13c231b2a6fefe7ad71074f9c52ac5e
Pablo Bezerra <Pdsbezerra1316@gmail.com>
2023-12-03 15:39:54 -0300

    modificando o script

--- js/comandos.js
+++ js/comandos.js
@@ -1,138 +1,75 @@
-// Edita o comportamento do container que revela os cards de projetos por area.
-function mostrarCards(n, condição=true){
-    let iGames = document.getElementById('iGames');
-    let iBusiness = document.getElementById('iBusiness');
-    let iExtras = document.getElementById('iExtras');
+(function () {
+    const header = document.querySelector('header')
+    const containers = document.querySelectorAll('.container')
+    const form = document.querySelector("#form")
+    const inputs = form.querySelectorAll('.required')
+    const p = form.querySelectorAll('.p_required')
+    let nome, email, mensagem = false
 
-    switch (n){
-        case 1:
-            if (Gamesalbum.className === 'album'){
-                Gamesalbum.className = 'revelar';
-                iGames.classList.add('rot')
-                Businessalbum.className = 'album';
-                iBusiness.classList.remove('rot')
-                Extrasalbum.className = 'album';
-                iExtras.classList.remove('rot')
-            }else{
-                Gamesalbum.className = 'album';
-                iGames.classList.remove('rot')
-            }
-            break;
-        case 2: 
-            if (Businessalbum.className === 'album'){
-                Gamesalbum.className= 'album';
-                iGames.classList.remove('rot')
-                Businessalbum.className = 'revelar';
-                iBusiness.classList.add('rot')
-                Extrasalbum.className = 'album';
-                iExtras.classList.remove('rot')
-            }else{
-                Businessalbum.className = 'album';
-                iBusiness.classList.remove('rot')
-            }
-            break;
-        case 3: 
-            if (Extrasalbum.className === 'album'){
-                Gamesalbum.className = 'album';
-                iGames.classList.remove('rot')
-                Businessalbum.className = 'album';
-                iBusiness.classList.remove('rot')
-                Extrasalbum.className = 'revelar';
-                iExtras.classList.add('rot')
-            }else{
-                Extrasalbum.className = 'album';
-                iExtras.classList.remove('rot')
-            }
-            break;
+    // Edita o comportamento do container que revela os cards de projetos por area.
+
+
+
+    // Edita o comportamento do menu da barra de menu.
+    function revelMenu(){
+        const menu = document.querySelector('#menu');
+        menu.classList.toggle('visivel');
     }
-    if (condição){revelMenu()};
-}
 
-// Edita o comportamento do menu da barra de menu.
-const revelMenu = () => {
-    let menu = document.querySelector('#menu');
-    menu.classList.toggle('visivel');
-}
+    // Edita o comportamento do carrossel.
 
-// Edita o comportamento do carrossel.
-const car = document.getElementById('hab');
-const divs = document.querySelectorAll('#hab > div');
-let cont = -2; 
-function carrossel(addcont=true){
-    if (addcont){
-        cont ++;
+
+    // configurando o envio da mensagem do formulário
+
+
+    function inserirErro(n){
+      inputs[n].style.border = '2px solid darkred'
+      p[n].style.display = 'block'
     }
-    if(cont >= divs.length -2 ){
-        cont = -2;
-    }else if( cont < -2){
-        cont = divs.length -2;
+
+    function removerErro(n){
+      inputs[n].style.border = ''
+      p[n].style.display = 'none'
     }
-    car.style.transform = `translateX(${-cont*290}px)`;
-    console.log
-}
-function passar(n) {
-    if (n === 1){
-        cont --;
-    }else{
-        cont ++;
+
+    function validarNome(){
+      if(inputs[0].value.length <= 3 || /\d/.test(inputs[0].value)){
+        inserirErro(0)
+        nome  = false
+      }else{
+        removerErro(0)
+        nome = true
+      }
+      ativarBotao()
     }
-    carrossel(false);
-}
-let atualizador = setInterval(carrossel, 10000); 
 
-// configurando o envio da mensagem do formulário
-const form = document.querySelector("#form")
-const inputs = form.querySelectorAll('.required')
-const p = form.querySelectorAll('.p_required')
-let nome, email, mensagem = false
+    function validarEmail(){
+      if( !/\S+@\S+\.\S+/.test(inputs[1].value)){
+        inserirErro(1)
+        email = false
+      }else{
+        removerErro(1)
+        email = true
+      }
+      ativarBotao()
+    }
 
-function inserirErro(n){
-  inputs[n].style.border = '2px solid darkred'
-  p[n].style.display = 'block'
-}
+    function validarMensagem(){
+      if (inputs[2].value.length <= 5){
+        inserirErro(2)
+        mensagem = false
+      }else{
+        removerErro(2)
+        mensagem = true
+      }
+      ativarBotao()
+    }
 
-function removerErro(n){
-  inputs[n].style.border = ''
-  p[n].style.display = 'none'
-}
-
-function validarNome(){
-  if(inputs[0].value.length <= 3 || /\d/.test(inputs[0].value)){
-    inserirErro(0)
-    nome  = false
-  }else{
-    removerErro(0)
-    nome = true
-  }
-  ativarBotao()
-}
-
-function validarEmail(){
-  if( !/\S+@\S+\.\S+/.test(inputs[1].value)){
-    inserirErro(1)
-    email = false
-  }else{
-    removerErro(1)
-    email = true
-  }
-  ativarBotao()
-}
-
-function validarMensagem(){
-  if (inputs[2].value.length <= 5){
-    inserirErro(2)
-    mensagem = false
-  }else{
-    removerErro(2)
-    mensagem = true
-  }
-  ativarBotao()
-}
-
-function ativarBotao(){
-  if(nome && email && mensagem){
-    botao.disabled = false
-  }else{
-    botao.disabled = true
-  }
-}
+    function ativarBotao(){
+      if(nome && email && mensagem){
+        botao.disabled = false
+      }else{
+        botao.disabled = true
+      }
+    }
+})()
--- js/organizador.js
+++ js/organizador.js
@@ -1,75 +1,189 @@
-// Cria os cards dos projetos.
-const areas = ['Games', 'Business', 'Extras'];
-const inserir = () => {
-    for (let a in areas){
-        const card = document.querySelectorAll(`#${areas[a]}album> .card`);
-        ciarCards(6 - card.length, areas[a]);
-    }
-}
-const ciarCards = (quantidade, local) => {
-    for (let n = 0; n < quantidade; n++){
-        const lugar = document.querySelector(`#${local}album`)
-        let div = document.createElement("div");
-        div.className = `card`;
-        div.innerHTML = `
-        <div>
-        <img src="./arquivos/imagens/contrução.jpg" alt="Em breve">
-        </div>
-        <div id="info">
-        <h3>Em Breve</h3>
-        <p>Mais novidades em breve.</p>
-        </div>
-        `;
-        lugar.appendChild(div);
-    }
-}
-inserir();
+(function(){
 
-// Configurações personalizado para cada tamanho de tela
-let limite = 0
-let altura = 0
-if(window.innerWidth <= 767){
-    limite = 25
-    altura = 60
-}else if (window.innerWidth >=767 && window.innerWidth < 992){
-    limite = 40
-    altura = 250
-}else if (window.innerWidth >= 992){
-    limite = 60
-    altura = 250
-}
+    const barra = document.querySelector('#barra')
+    const menu = document.querySelector('#menu')
+    const triangulo = document.querySelector('.triangulos')
+    let limite = 0
+    let altura = 0
+    const ids = ['face', 'insta', 'twitter', 'linkedin', 'github']
+    const cardHabilidades = [
+        {
+            nome: 'Python',
+            descricao: 'É amplamente usada no desenvolvimento web, automação, análise de dados e muito mais.',
+            img: './arquivos/icones/icon_python.png'
+        },
+        {
+            nome: 'Html',
+            descricao: '(Hypertext Markup Language) é a linguagem padrão para criar páginas da web',
+            img: './arquivos/icones/icon_html.png'
+        },
+        {
+            nome: 'Css',
+            descricao: '(Cascading Style Sheets),é uma linguagem poderosa para personalizar a estética das páginas web.',
+            img: './arquivos/icones/icon_css.png'
+        },
+        {
+            nome: 'JavaScript',
+            descricao: ' É uma linguagem de programação fundamental para o desenvolvimento web',
+            img: './arquivos/icones/icon_js.png'
+        },
+        {
+            nome: 'Em formação',
+            descricao: 'React, Bootstrap, NodeJs, MySQL',
+            img: './arquivos/icones/icon_atenção.png'
+        }
+    ]
+    const cardsProjetos = [
+        {   
+            destino: 'Games',
+            nome:'Jogo da Velha',
+            descricao: 'O clássico jogo da velha feito em Python com PyQty6.',
+            links:['https://github.com/PabloBezerra/Primeiro-game'],
+            imagem: './arquivos/imagens/projeto_jogodavelha.jpg',
+            alt: 'Projeto jogo da velha'
+        },
+        {
+            destino: 'Business',
+            nome:'Cartão Interativo',
+            descricao: 'Desafio do Frontend Mentor de cartão de crédito interativo.',
+            links:['https://github.com/PabloBezerra/Cartao_interativo', 'https://pablobezerra.github.io/Cartao_interativo/'],
+            imagem: './arquivos/imagens/projeto_cartaoInterativo.jpg',
+            alt: 'Projeto Cartão Interativo'
+        },
+        {
+            destino: 'Business',
+            nome:'Ecommerce Product Page',
+            descricao: 'Desafio do Frontend Mentor de uma página de um produto comercial.',
+            links:["https://github.com/PabloBezerra/Ecommerce-product-page", "https://pablobezerra.github.io/Ecommerce-product-page/" ],
+            imagem: './arquivos/imagens/projeto_Ecommerce-page.jpg',
+            alt: 'Projeto página de produto comercial'
+        },
+        {
+            destino: 'Extras',
+            nome:'LogOff',
+            descricao: 'Agenda um desligamento ou reinicialização do windows, feito em Python com PyQty6.',
+            links:["https://github.com/PabloBezerra/logoff" ],
+            imagem: './arquivos/imagens/projeto_logoff.jpg',
+            alt: 'Projeto LogOff'
+        },
+        {
+            destino: 'Extras',
+            nome:'Gerenciador de Tarefas',
+            descricao: 'Desafio do Frontend Mentor de Gerenciador de Tarefas',
+            links:["https://github.com/PabloBezerra/Gerenciador_tarefas", "https://pablobezerra.github.io/Gerenciador_tarefas/"],
+            imagem: './arquivos/imagens/projeto_gerenciadordeTarefas.jpg',
+            alt: 'Projeto Gerenciador de tarefas'
+        }
+    ]
 
-// Edita o comportamento da barra de menu conforme o scroll da tela.
-let barra = document.querySelector('#barra');
-let menu = document.querySelector('#menu')
+    // Cria os cards das habilidades
+    cardHabilidades.forEach(element =>{
+        const div = document.createElement('div')
 
-window.addEventListener('scroll', ()=> {
-    barra.classList.toggle('novo',(window.scrollY > altura))
-    menu.classList.remove('visivel', (window.scrollY > altura))
-})
+        const img = document.createElement('img')
+        img.src = element.img
+        img.alt = element.nome
 
-// Edita o comportamento da barra de navegação das redes sociais.
-const ids = ['face', 'insta', 'twitter', 'linkedin', 'github'];
-for(let i in ids){
-    let id = document.querySelector(`#${ids[i]}`);
-    id.addEventListener('mouseover', () => {
-        id.src = `./arquivos/icones/icon_${ids[i]}_over.png`;
+        const p = document.createElement('p')
+        p.innerHTML = `<em>${element.nome}</em> <br> ${element.descricao}`
+
+        div.appendChild(img)
+        div.appendChild(p)
+
+        const amontoado = document.querySelector('.amontoado')
+        amontoado.appendChild(div)
     })
-    id.addEventListener('mouseout', () => {
-        id.src = `./arquivos/icones/icon_${ids[i]}.png`;
+
+    // Cria os cards dos projetos.
+    cardsProjetos.forEach(element => {
+        const divCard = document.createElement('div')
+        divCard.classList.add('card')
+
+        const img = document.createElement('img')
+        img.src = element.imagem
+        img.alt = element.alt
+
+        const divInfo = document.createElement('div')
+        divInfo.classList.add('info')
+
+        const h3 = document.createElement('h3')
+        h3.innerText = element.nome
+
+        const p = document.createElement('p')
+        p.innerText = element.descricao
+
+        const divLink = document.createElement('div')
+        divLink.classList.add('links')
+
+        const a1 = document.createElement('a')
+        a1.href = element.links[0]
+        a1.target = '_blank'
+        a1.innerText = 'Ver Código'
+
+        divLink.appendChild(a1)
+        if(element.links.length === 2){
+            const a2 = document.createElement('a')
+            a2.href = element.links[1]
+            a2.target='_blank'
+            a2.innerText = 'Ver Página'
+
+            divLink.appendChild(a2)
+        }
+
+        divInfo.appendChild(h3)
+        divInfo.appendChild(p)
+        divInfo.appendChild(divLink)
+
+        divCard.appendChild(img)
+        divCard.appendChild(divInfo)
+
+        const album = document.querySelector(`.${element.destino}`).lastElementChild
+        album.appendChild(divCard)
+
+    });
+
+    // Configurações personalizado para cada tamanho de tela
+
+    if(window.innerWidth <= 767){
+        limite = 25
+        altura = 60
+    }else if (window.innerWidth >=767 && window.innerWidth < 992){
+        limite = 40
+        altura = 250
+    }else if (window.innerWidth >= 992){
+        limite = 60
+        altura = 250
+    }
+
+    // Edita o comportamento da barra de menu conforme o scroll da tela.
+
+    window.addEventListener('scroll', ()=> {
+        barra.classList.toggle('novo',(window.scrollY > altura))
+        menu.classList.remove('visivel', (window.scrollY > altura))
     })
-}
 
-// Cria os triangulos para a animação de fundo
-const triangulo = document.querySelector('.triangulos')
+    // Edita o comportamento da barra de navegação das redes sociais.
 
-for(let n = 0; n <= limite; n++){
-    let span = document.createElement('span')
-    span.style = `--i:${getRandomInt(1,30)}`
-    triangulo.appendChild(span)
-}
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
+    for(let i in ids){
+        let id = document.querySelector(`#${ids[i]}`);
+        id.addEventListener('mouseover', () => {
+            id.src = `./arquivos/icones/icon_${ids[i]}_over.png`;
+        })
+        id.addEventListener('mouseout', () => {
+            id.src = `./arquivos/icones/icon_${ids[i]}.png`;
+        })
+    }
+
+    // Cria os triangulos para a animação de fundo
+
+    for(let n = 0; n <= limite; n++){
+        const span = document.createElement('span')
+        span.style = `--i:${getRandomInt(1,30)}`
+        triangulo.appendChild(span)
+    }
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+})()
